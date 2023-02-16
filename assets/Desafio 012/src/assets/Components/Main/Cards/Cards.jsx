be0e2b6d108bba8914270f4bb88770d06c382995
israelrodrigues01 <ciceroisrael428@gmail.com>
2023-02-16 15:19:24 -0300

    feat: insiro o import das imagens nos cards

--- assets/Desafio 012/src/assets/Components/Main/Cards/Cards.jsx
+++ assets/Desafio 012/src/assets/Components/Main/Cards/Cards.jsx
@@ -1,50 +1,55 @@
 import Card from './Card'
+import pizza1 from '../../../img/pizza1.png'
+import pizza2 from '../../../img/pizza2.png'
+import pizza3 from '../../../img/pizza3.png'
+import pizza4 from '../../../img/pizza4.png'
 
-function Cards(){
+
+function Cards() {
 
 	const cardsInfor = [
 		{
-	        id: 1,
-	        image: 'image 16.png',
-	        title: 'La Cuma',
-	        desc: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.',
-	        preco: 'R$ 23',
-	        btn: 'Comprar agora'
-	    },
-	    {
-	        id: 2,
-	        image: 'image 17.png',
-	        title: 'La Cuma',
-	        desc: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.',
-	        preco: 'R$ 23',
-	        btn: 'Comprar agora'
-	    },
-	    {
-	        id: 3,
-	        image: 'image 18.png',
-	        title: 'La Cuma',
-	        desc: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.',
-	        preco: 'R$ 23',
-	        btn: 'Comprar agora'
-	    },
-	    {
-	        id: 4,
-	        image: 'image 19.png',
-	        title: 'La Cuma',
-	        desc: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.',
-	        preco: 'R$ 23',
-	        btn: 'Comprar agora'
-	    },
+			id: 1,
+			image: pizza1,
+			title: 'La Cuma',
+			desc: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.',
+			preco: 'R$ 23',
+			btn: 'Comprar agora'
+		},
+		{
+			id: 2,
+			image: pizza2,
+			title: 'La Menta',
+			desc: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.',
+			preco: 'R$ 23',
+			btn: 'Comprar agora'
+		},
+		{
+			id: 3,
+			image: pizza3,
+			title: 'La Bana',
+			desc: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.',
+			preco: 'R$ 23',
+			btn: 'Comprar agora'
+		},
+		{
+			id: 4,
+			image: pizza4,
+			title: 'La Toca',
+			desc: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.',
+			preco: 'R$ 23',
+			btn: 'Comprar agora'
+		},
 	];
 
-	return(
+	return (
 		<div className="content-cards">
 			{cardsInfor.map(card => (
-				<Card 
-					title={card.title} 
-					image={`src/assets/img/${card.image}`} 
-					desc={card.desc} 
-					preco={card.preco} 
+				<Card
+					title={card.title}
+					image={card.image}
+					desc={card.desc}
+					preco={card.preco}
 					button={card.btn} />
 			))}
 		</div>
